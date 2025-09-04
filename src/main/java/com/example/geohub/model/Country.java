167bb14c782a57567cbb0d3cedc86b2b3b5fbc58
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-04 17:04:16 +0000

    Implemented Country Model entity

--- src/main/java/com/example/geohub/model/Country.java
+++ src/main/java/com/example/geohub/model/Country.java
@@ -1,9 +1,67 @@
-/*
- *
- * You can use the following import statements
- * 
- * import javax.persistence.*;
- * 
- */
+package com.example.geohub.model;
 
-// Write your code here
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int countryId;
+
+    private String countryName;
+    private String currency;
+    private long population;
+    private String latitude;
+    private String longitude;
+
+    // Getters and Setters
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
