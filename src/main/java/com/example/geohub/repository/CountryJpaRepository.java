a33e896f6d5320470fa63ecd556237a1f47d69cf
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-05 16:44:04 +0000

    Implemented repositories of Country Entity

--- src/main/java/com/example/geohub/repository/CountryJpaRepository.java
+++ src/main/java/com/example/geohub/repository/CountryJpaRepository.java
@@ -1,10 +1,9 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.data.jpa.repository.JpaRepository;
- * import org.springframework.stereotype.Repository;
- * 
- */
+package com.example.geohub.repository;
 
-// Write your code here
+import com.example.geohub.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryJpaRepository extends JpaRepository<Country, Integer> {
+}
--- src/main/java/com/example/geohub/repository/CountryRepository.java
+++ src/main/java/com/example/geohub/repository/CountryRepository.java
@@ -1,9 +1,12 @@
-/*
- *
- * You can use the following import statements
- * 
- * java.util.ArrayList;
- * 
- */
+package com.example.geohub.repository;
 
-// Write your code here
+import com.example.geohub.model.Country;
+import java.util.List;
+
+public interface CountryRepository {
+    List<Country> findAllCountries();
+    Country findCountryById(int countryId);
+    Country saveCountry(Country country);
+    Country updateCountry(int countryId, Country country);
+    void deleteCountry(int countryId);
+}
