145fe771b7eaa0153b14308aaaa7d550a270c077
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-14 18:06:35 +0000

    Implemented countryController API

--- src/main/java/com/example/geohub/controller/CountryController.java
+++ src/main/java/com/example/geohub/controller/CountryController.java
@@ -9,3 +9,50 @@
  */
 
 // Write your code here
+
+package controller;
+
+import model.Country;
+import service.CountryJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryJpaService countryService;
+
+    // Get all countries
+    @GetMapping
+    public ArrayList<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+
+    // Get country by ID
+    @GetMapping("/{id}")
+    public Country getCountryById(@PathVariable int id) {
+        return countryService.getCountryById(id);
+    }
+
+    // Create new country
+    @PostMapping
+    public Country addCountry(@RequestBody Country country) {
+        return countryService.addCountry(country);
+    }
+
+    // Update country
+    @PutMapping("/{id}")
+    public Country updateCountry(@PathVariable int id, @RequestBody Country country) {
+        return countryService.updateCountry(id, country);
+    }
+
+    // Delete country
+    @DeleteMapping("/{id}")
+    public void deleteCountry(@PathVariable int id) {
+        countryService.deleteCountry(id);
+    }
+}
